f2a5bdbd6f7a5830c490069061ace24c3a767149
Mikhail Voronov <msvoronov@inbox.ru>
2024-08-19 13:04:33 +0300

    modified brain-games.js, create cli.js

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export const greetings = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + userName + '!');    
+};
