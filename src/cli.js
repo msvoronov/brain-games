b9618ecad0f4cc6577ff0c34f0c1205e29610f5e
Mikhail Voronov <msvoronov@inbox.ru>
2024-08-20 23:34:41 +0300

    install eslint & fix problem

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greetings = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');    
+const greetings = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
+
+export default greetings;
